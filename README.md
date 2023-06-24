e3c6c35d4434496b41babef3a27f4d3af199486a
lastemp <emmanuelochieng797@gmail.com>
2023-06-24 23:09:34 +0300

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # mysql
+
+This RESTful Actix Web API examples illustrates how to connect to MySQL database using Mysql client library implemented in rust i.e  MySql database driver
+
+Currently this RESTful API supports: 
+- Add Bank
+- Add Branch
+- Add Teller
+- Add Customer
+- Get Bank
+- Get Branch
+- Get Teller
+- Get Customer
+
+The RESTful Actix Web API has below listed dependencies:
+- [Actix Web](https://github.com/actix/actix-web) web framework for Rust
+- [Serde](https://github.com/serde-rs/serde) for serializing and deserializing Rust data structures
+- [MySQL](https://github.com/mysql/mysql-server) MySQL database server
+- [mysql](https://github.com/blackbeam/rust-mysql-simple) MySql database driver
+
+## Running The RESTful Actix Web API
+
+First, open the terminal and clone the project using `git` (or you can simply download the project) and then change the directory:
+
+```Rust
+git clone https://github.com/lastemp/mysql.git
+cd mysql
+```
+
+```MySQL
+Open folder "mysql\sql" that contains the SQL files to create the tables in your local MySQL database server.
+Please ensure you execute the SQL statements found in the files in your MySQL database server.
+```
+
+```Config file
+Open file "xxx" located in path "mysql\xxx" and then 
+change the configurations to match the credentials of your MySQL database server.
+```
+
+Once you are inside the project directory, run the application:
+
+```Rust
+cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,241 @@
+mod db_layer;
+
+use actix_web::{get, post, web, App, HttpServer, Responder};
+use mysql::*;
+use serde::{Deserialize, Serialize};
+use std::str;
+
+#[derive(Deserialize)]
+struct BankData {
+    bank_name: String,
+    country: String,
+}
+
+#[derive(Deserialize)]
+struct BranchData {
+    branch_name: String,
+    location: String,
+}
+
+#[derive(Deserialize)]
+struct TellerData {
+    teller_name: String,
+    branch_name: String,
+}
+
+#[derive(Deserialize)]
+struct CustomerData {
+    customer_name: String,
+    branch_name: String,
+}
+
+// output
+#[derive(Serialize)]
+struct ApiResponseData {
+    pub status_code: u8,
+    pub status_description: String,
+}
+
+#[derive(Serialize)]
+pub struct BankDetails {
+    pub bank_name: String,
+    pub country: String,
+}
+
+#[derive(Serialize)]
+pub struct BankResponseData {
+    pub status_code: u8,
+    pub status_description: String,
+    pub bank_data: Vec<BankDetails>,
+}
+
+#[derive(Serialize)]
+pub struct BranchDetails {
+    pub branch_name: String,
+    pub location: String,
+}
+
+#[derive(Serialize)]
+pub struct BranchResponseData {
+    pub status_code: u8,
+    pub status_description: String,
+    pub branch_data: Vec<BranchDetails>,
+}
+
+#[derive(Serialize)]
+pub struct TellerDetails {
+    pub teller_name: String,
+    pub branch_name: String,
+}
+
+#[derive(Serialize)]
+pub struct TellerResponseData {
+    pub status_code: u8,
+    pub status_description: String,
+    pub teller_data: Vec<TellerDetails>,
+}
+
+#[derive(Serialize)]
+pub struct CustomerDetails {
+    pub customer_name: String,
+    pub branch_name: String,
+}
+
+#[derive(Serialize)]
+pub struct CustomerResponseData {
+    pub status_code: u8,
+    pub status_description: String,
+    pub customer_data: Vec<CustomerDetails>,
+}
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    format!("")
+}
+
+#[post("/addbank")]
+async fn add_bank(bank_data: web::Json<BankData>, data: web::Data<Pool>) -> impl Responder {
+    let bank_name = &bank_data.bank_name;
+    let _country = &bank_data.country;
+
+    let response_data = db_layer::create_bank(&data, bank_name.to_string(), _country.to_string());
+
+    let api_response_data: ApiResponseData = ApiResponseData {
+        status_code: response_data.status_code,
+        status_description: response_data.status_description,
+    };
+
+    web::Json(api_response_data)
+}
+
+#[post("/addbranch")]
+async fn add_branch(branch_data: web::Json<BranchData>, data: web::Data<Pool>) -> impl Responder {
+    let branch_name = &branch_data.branch_name;
+    let _location = &branch_data.location;
+
+    let response_data =
+        db_layer::create_branch(&data, branch_name.to_string(), _location.to_string());
+
+    let api_response_data: ApiResponseData = ApiResponseData {
+        status_code: response_data.status_code,
+        status_description: response_data.status_description,
+    };
+
+    web::Json(api_response_data)
+}
+
+#[post("/addteller")]
+async fn add_teller(teller_data: web::Json<TellerData>, data: web::Data<Pool>) -> impl Responder {
+    let teller_name = &teller_data.teller_name;
+    let branch_name = &teller_data.branch_name;
+
+    let response_data =
+        db_layer::create_teller(&data, teller_name.to_string(), branch_name.to_string());
+
+    let api_response_data: ApiResponseData = ApiResponseData {
+        status_code: response_data.status_code,
+        status_description: response_data.status_description,
+    };
+
+    web::Json(api_response_data)
+}
+
+#[post("/addcustomer")]
+async fn add_customer(
+    customer_data: web::Json<CustomerData>,
+    data: web::Data<Pool>,
+) -> impl Responder {
+    let customer_name = &customer_data.customer_name;
+    let branch_name = &customer_data.branch_name;
+
+    let response_data =
+        db_layer::create_customer(&data, customer_name.to_string(), branch_name.to_string());
+
+    let api_response_data: ApiResponseData = ApiResponseData {
+        status_code: response_data.status_code,
+        status_description: response_data.status_description,
+    };
+
+    web::Json(api_response_data)
+}
+
+#[get("/getbank")]
+async fn get_bank(data: web::Data<Pool>) -> impl Responder {
+    let bank_response_data = db_layer::get_bank_data(&data);
+
+    web::Json(bank_response_data)
+}
+
+#[get("/getbranch")]
+async fn get_branch(data: web::Data<Pool>) -> impl Responder {
+    let branch_response_data = db_layer::get_branch_data(&data);
+
+    web::Json(branch_response_data)
+}
+
+#[get("/getteller")]
+async fn get_teller(data: web::Data<Pool>) -> impl Responder {
+    let teller_response_data = db_layer::get_teller_data(&data);
+
+    web::Json(teller_response_data)
+}
+
+#[get("/getcustomer")]
+async fn get_customer(data: web::Data<Pool>) -> impl Responder {
+    let customer_response_data = db_layer::get_customer_data(&data);
+
+    web::Json(customer_response_data)
+}
+
+fn get_conn_builder() -> OptsBuilder {
+    let builder = OptsBuilder::new()
+        .ip_or_hostname(Some("localhost"))
+        .db_name(Some("databasename"))
+        .user(Some("databaseuser"))
+        .pass(Some("databasepassword"));
+    builder
+}
+
+#[actix_web::main]
+async fn main() {
+    let builder: OptsBuilder = get_conn_builder();
+    let pool = match Pool::new(builder) {
+        Ok(pool) => pool,
+        Err(e) => {
+            println!("Failed to open DB connection. {:?}", e);
+            return;
+        }
+    };
+
+    let shared_data = web::Data::new(pool);
+
+    let server = match HttpServer::new(move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .service(greet)
+            .service(add_bank)
+            .service(add_branch)
+            .service(add_teller)
+            .service(add_customer)
+            .service(get_bank)
+            .service(get_branch)
+            .service(get_teller)
+            .service(get_customer)
+    })
+    .bind("0.0.0.0:9247")
+    {
+        Ok(s) => {
+            println!("[info] ActixWebHttpServer - Listening for HTTP on /0.0.0.0:9247");
+            s
+        }
+        Err(e) => {
+            println!("Failed to bind port. {:?}", e);
+            return;
+        }
+    };
+
+    match server.run().await {
+        Ok(_) => println!("Server exited normally."),
+        Err(e) => println!("Server exited with error: {:?}", e),
+    };
+}
